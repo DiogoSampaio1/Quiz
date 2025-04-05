351fa6886d6061b6a273b78c17ed10d0a585a335
DiogoSampaio1 <sampaio.diogo2007@gmail.com>
2025-04-05 14:51:00 +0100

    commit changes

--- database.js
+++ database.js
@@ -1,16 +1,24 @@
 const mongoose = require("mongoose");
 
-function connectToDatabase() {
-    mongoose.connect(process.env.DATABASE_URL,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }
-    );
+async function connectToDatabase() {
+  try {
+    // Verificar se a conexão já foi estabelecida
+    if (mongoose.connections[0].readyState) {
+      console.log("🌕 Já estamos conectados ao banco de dados.");
+      return;
+    }
 
-    const db = mongoose.connection;
-    db.on("error", (error) => console.error(error));
-    db.once("open", () => console.log("🌕 Connected to the database!"));
+    // Conectar ao banco de dados usando a URL fornecida no arquivo .env
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("🌕 Conectado ao banco de dados com sucesso!");
+  } catch (error) {
+    console.error("❌ Erro ao conectar ao banco de dados:", error);
+    throw new Error("Erro ao conectar ao banco de dados");
+  }
 }
 
 module.exports = connectToDatabase;
